c08d804b23c0cfa881639c9b48ab411648bac007
Pistachio Guoguo <sangzizhou@gmail.com>
2021-02-06 16:26:43 -0600

    simplfied some grammar, img part is omitted

--- weather_app/scripts/app.js
+++ weather_app/scripts/app.js
@@ -6,11 +6,41 @@ const updateCity = async (city) => {
     const cityDets = await getCity(city);  // city details
     const weather = await getWeather(cityDets.Key);
 
-    return {
-        cityDets,
-        weather
-    };
+    return { cityDets,weather}; 
+     // object shorthand notation  key1:key1, key2:key2 => key1, key2
 };
+
+
+
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    const {cityDets, weather} = data; // destructure
+    
+    details.innerHTML = ` <h5 class="my-3">${cityDets.EnglishName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>`;
+
+    if (card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+
+};
+
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+
+
+
 
 
 // when press enter to submit, send the city
@@ -22,6 +52,9 @@ cityForm.addEventListener('submit', e=>{
     const city = cityForm.city.value.trim();
     cityForm.reset();
 
-    updateCity(city).then(data => console.log(data))
+    updateCity(city).then(data => updateUI(data))
         .catch(err => console.log(err));
-})
+});
+
+
+
